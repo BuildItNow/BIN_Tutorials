2acbebb1aca7b347212f379112fc7bf145f01b8e
Yang G <pinner@mail.ustc.edu.cn>
2015-09-28 16:36:52 +0800

    添加Tutorials源代码

---
+++ application/application.js
@@ -0,0 +1,18 @@
+define(
+["bin/core/application", "application/dataCenter", "css!application/css/demo.css"],
+function(Base, DataCenter)
+{
+	var Application = {};
+
+	Application.init = function()
+	{
+		Base.prototype.init.call(this);
+	}
+
+	Application.run = function()
+	{
+		bin.naviController.startWith("welcome/index");
+	}
+
+	return Base.extend(Application);
+});
