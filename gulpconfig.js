af1b6e1d13b8484b9cc4531344c86c0c5cca7eae
Yang G <pinner@mail.ustc.edu.cn>
2016-04-06 22:49:17 +0800

    添加自动构建配置文件

---
+++ gulpconfig.js
@@ -0,0 +1,42 @@
+module.exports = 
+{
+	dest:"dest",
+	lsCaches:
+	{
+        //all:true,
+    	dirs:
+    	[
+            // directory or files
+            "alertDemo",
+            "application",
+            "baiduMapDemo",
+            "cacheDemo",
+            "common",
+            "dataCenterDemo",
+            "datePickerDemo",
+            "hybridDemo",
+            "indicatorDemo",
+            "IOAnimCallbackDemo",
+            "lazyLoadDemo",
+            "lazyLoadListDemo",
+            "lsLoaderDemo",
+            "naviPageDemo",
+            "netDemo",
+            "netLocalDemo",
+            "normalPageDemo",
+            "pageJumpDemo",
+            "pageJumpParamsDemo",
+            "prLoaderDemo",
+            "refreshListViewDemo",
+            "refreshViewDemo",
+            "remDemo",
+            "sendCheckDemo",
+            "statusDemo",
+            "swipeDemo",
+            "switchDemo",
+            "tabBarDemo",
+            "viewDemo",
+            "welcome",
+    	],
+	}
+}
