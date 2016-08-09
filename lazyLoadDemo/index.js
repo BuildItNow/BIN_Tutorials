3cb40b773b8f3c0416c7cc148b08be5712629ad3
Yang G <pinner@mail.ustc.edu.cn>
2016-08-09 22:49:27 +0800

    BUG FIX

--- lazyLoadDemo/index.js
+++ lazyLoadDemo/index.js
@@ -4,13 +4,6 @@ define(
 	{
 		var Class = {};
 
-		Class.posGenHTML = function()
-		{
-			Base.prototype.posGenHTML.call(this);
-
-			new bin.ui.View({elem:this.$content()});
-		}
-
 		return Base.extend(Class);
 	}
 );
